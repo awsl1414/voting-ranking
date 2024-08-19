f7f0a84381df15cccb0026f691c249dbb4480288
awsl1414 <3030994569@qq.com>
2024-08-19 23:34:35 +0800

    feat: :sparkles: 用户表设计

---
+++ common/util/times.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: awsl1414 3030994569@qq.com
+ * @Date: 2024-08-19 23:20:44
+ * @LastEditors: awsl1414 3030994569@qq.com
+ * @LastEditTime: 2024-08-19 23:27:48
+ * @FilePath: /voting-ranking/common/util/times.go
+ * @Description:
+ *
+ */
+package util
+
+import "time"
+
+type HTime struct {
+	time.Time
+}
