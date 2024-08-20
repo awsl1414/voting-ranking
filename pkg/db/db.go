ebd797f5d5a655ba29c04c89f1709de18731f270
awsl1414 <3030994569@qq.com>
2024-08-20 22:51:50 +0800

    feat: :sparkles: 自动检查并迁移表

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,8 +1,18 @@
+/*
+ * @Author: awsl1414 3030994569@qq.com
+ * @Date: 2024-08-19 16:54:03
+ * @LastEditors: awsl1414 3030994569@qq.com
+ * @LastEditTime: 2024-08-20 17:50:45
+ * @FilePath: /voting-ranking/pkg/db/db.go
+ * @Description:
+ *
+ */
 package db
 
 import (
 	"fmt"
 	"time"
+	"voting-ranking/api/model"
 	"voting-ranking/common/config"
 
 	"gorm.io/driver/mysql"
@@ -14,9 +24,10 @@ var Db *gorm.DB
 
 // 数据库初始化
 func SetupDBLink() error {
-	var err error
+
 	var dbConfig = config.Config.Db
 
+	// 初始化不带数据库名的 DSN
 	dsnNoDB := fmt.Sprintf("%s:%s@tcp(%s:%d)/?charset=%s&parseTime=True&loc=Local",
 		dbConfig.Username,
 		dbConfig.Password,
@@ -25,7 +36,7 @@ func SetupDBLink() error {
 		dbConfig.Charset,
 	)
 
-	Db, err = gorm.Open(mysql.Open(dsnNoDB), &gorm.Config{
+	Db, err := gorm.Open(mysql.Open(dsnNoDB), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent), // 设置 Gorm 的日志记录级别为“Silent”。
 	})
 	if err != nil {
@@ -51,7 +62,7 @@ func SetupDBLink() error {
 		fmt.Println("Database already exists:", dbConfig.Db)
 	}
 
-	// 连接到数据库
+	// 重新连接到指定数据库
 	dsnWithDB := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local",
 		dbConfig.Username,
 		dbConfig.Password,
@@ -65,6 +76,8 @@ func SetupDBLink() error {
 	if err != nil {
 		panic(err)
 	}
+
+	// 配置数据库连接池
 	sqlDB, err := Db.DB()
 	if err != nil {
 		panic(err)
@@ -72,6 +85,28 @@ func SetupDBLink() error {
 	sqlDB.SetMaxIdleConns(dbConfig.MaxIdle)
 	sqlDB.SetMaxOpenConns(dbConfig.MaxOpen)
 
+	// 检查并创建表
+	tables := map[string]interface{}{
+		"user": &model.User{},
+	}
+	for tableName, tableModel := range tables {
+		var tableExists bool
+		query := "SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_schema = ? AND table_name = ?)"
+		err = Db.Raw(query, dbConfig.Db, tableName).Scan(&tableExists).Error
+		if err != nil {
+			return fmt.Errorf("failed to check table '%s' existence: %w", tableName, err)
+		}
+
+		if !tableExists {
+			fmt.Printf("Table '%s' does not exist, creating...", tableName)
+			err := Db.AutoMigrate(tableModel)
+			if err != nil {
+				return fmt.Errorf("failed to create table '%s': %w", tableName, err)
+			}
+		} else {
+			fmt.Printf("Table '%s' already exists, skipping creation.", tableName)
+		}
+	}
 	return nil
 
 }
